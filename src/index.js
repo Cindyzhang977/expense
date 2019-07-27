b787043f0c05a56c397a623813419e79af4d684a
Cindy <cindyzhang977@gmail.com>
2019-07-27 12:58:00 -0700

    started handling submit for sign up

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import logo from "./imgs/logo.png";
-console.log(logo);
 
 class HomePage extends React.Component {
   constructor(props) {
@@ -53,15 +52,68 @@ function Navbar(props) {
 }
 
 class Signup extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    // this.state = {
+    //   firstname=null,
+    //   //continue...
+    // };
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const data = new FormData(form);
+
+    for (let name of data.keys()) {
+      console.log(data.keys());
+      const input = form.elements[name];
+      // const parserName = input.dataset.parse;
+      // console.log(parserName);
+      data.set(name, data.get(name));
+      console.log(name + " " + data.get(name));
+      // if (parserName) {
+      //   const parser = inputParsers[parserName];
+      //   const parsedValue = parser(data.get(name));
+      //   data.set(name, parsedValue);
+      // }
+    }
+
+    // const temp = {name: "Cindy"};
+    // console.log(temp);
+    fetch('/api/form-submit-url', {
+      method: 'POST',
+      body: data,
+    })
+    .then(res => console.log(res));
+    // .then(res => console.log(res))
+    // .then(temp => console.log("success!!", JSON.stringify(temp)))
+    // .then((response) => console.log('Success: ', JSON.stringify(response)))
+    // .catch(err => console.log(err));
+
+    // .then(function (res) {
+    //   console.log(res);
+    //     return res, data
+    // })
+    // .then(function (data) {
+    //     console.log("data " + data);
+    // }).catch(function (err) {
+    //     console.log(err)
+    // })
+    //
+    // fetch('/api/form-submit-url', {
+    //   method: 'GET',
+    // })
+    // .then((response) => console.log('Success: ', JSON.stringify(response)));
+    //response is param for this function that stringifies response json file (aka the data)
+  }
 
   render() {
     return (
       <div className="form-container">
           <div className="heading"><h1>Sign Up</h1></div>
-          <form action="[specify form handler, what to do with form once submitted]">
+          <form onSubmit={this.handleSubmit}>
               <div className="name">
                   <div id="first-name">
                       First Name <br />
