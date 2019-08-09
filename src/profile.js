f46111cceef9a27cd0899355c1ae77897e31f216
Cindy <cindyzhang977@gmail.com>
2019-08-09 14:22:38 -0700

    mobile profile css

--- src/profile.js
+++ src/profile.js
@@ -2,13 +2,23 @@ import React from 'react';
 import "./profile.css";
 
 class Profile extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      firstname: 'First',
+      lastname: 'Last',
+      email: 'email@email.com',
+      username: 'username',
+      password: 'password',
+    };
+  }
   render() {
     return (
       <div>
         <div className="content">
             <h1>Profile Basic Information</h1>
         </div>
-        <ProfileInfo />
+        <ChangeProfileInfo />
         <div className="content">
             <h1>Reset Password</h1>
         </div>
@@ -18,26 +28,26 @@ class Profile extends React.Component {
   }
 }
 
-function ProfileInfo(props) {
+function ChangeProfileInfo(props) {
   return (
     <div className="profile-info content">
         <div className="info">
-            <div>
+            <div className="info-items">
                 <p>First Name:</p>
                 <p>Last Name:</p>
                 <p>Email:</p>
                 <p>Username:</p>
             </div>
             <form>
-                <input type="text" value="First Name" />
-                <input type="text" value="Last Name" />
-                <input type="text" value="Email" />
-                <input type="text" value="Username" />
+                <input type="text" value="First Name" /><br />
+                <input type="text" value="Last Name" /><br />
+                <input type="text" value="Email" /><br />
+                <input type="text" value="Username" /><br />
             </form>
         </div>
         <div className="edit-buttons">
-            <button className="button">Edit</button> <br />
-            <button className="button">Cancel</button>
+            <button className="button" id="cancel">Cancel</button> <br />
+            <button className="button edit">Edit</button>
         </div>
     </div>
   );
@@ -45,16 +55,16 @@ function ProfileInfo(props) {
 
 function ResetPassword(props) {
   return (
-    <form className="form-container content">
-        Old Password:
-        <input type="text" name="old-password" />
-        New Password:
-        <input type="text" name="new-password" />
-        Confirm New Password:
+    <form className="form-container">
+        Old Password: <br />
+        <input type="text" name="old-password" /><br />
+        New Password: <br />
+        <input type="text" name="new-password" /><br />
+        Confirm New Password: <br />
         <input type="text" />
         <div className="edit-buttons">
-            <button className="cancel-button">Cancel</button>
-            <input type="submit" value="Change Password" />
+            <button className="button" id="cancel">Cancel</button><br />
+            <button className="button edit" id="change-pass">Change Password</button><br />
         </div>
 
     </form>
