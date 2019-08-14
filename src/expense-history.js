d9b6ac673d3d63287018fb31ddd382d9d2c1febc
Cindy <cindyzhang977@gmail.com>
2019-08-13 22:10:18 -0700

    toggle expense history graphs

--- src/expense-history.js
+++ src/expense-history.js
@@ -5,28 +5,43 @@ class ExpenseHistory extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeTab: "month",
+      activeTab: "Week",
+      graphs: {"Week": <WeekGraph />, "Month": <MonthGraph />, "Year": <YearGraph />},
     }
   }
 
+  changeActiveTab(newTab) {
+    this.setState({activeTab: newTab});
+  }
+
   render() {
+    var tabs = ["Week", "Month", "Year"].map((id) => {
+        var classname = "tab";
+        if (id === this.state.activeTab) {
+          classname = "tab-selected";
+        }
+        return <Tab className={classname} id={id} changeActiveTab={this.changeActiveTab.bind(this)} />;
+    });
+
     return (
       <div className="content">
         <h1>Expense History</h1>
         <p>Here is a graph to visualize your past expenses. Choose to see trends for this week, this month, or this year.</p>
-        <div class="tracker-container">
-            <ul class="tabs">
-                <li class="tab-selected" id="week">Week</li>
-                <li class="tab" id="month">Month</li>
-                <li class="tab" id="year">Year</li>
+        <div className="tracker-container">
+            <ul className="tabs">
+                {tabs}
             </ul>
-          <div class="graph">
-            <h3>WEEK SUMMARY</h3>
+          <div className="graph">
+            {this.state.graphs[this.state.activeTab]}
           </div>
         </div>
       </div>
     );
   }
+}
+
+function Tab(props) {
+  return <li className={props.className} id={props.id} onClick={() => {props.changeActiveTab(props.id)}}>{props.id}</li>
 }
 
 class WeekGraph extends React.Component {
